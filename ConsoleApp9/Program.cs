78f5034bd67155a9f83389a4f42a6bbf9a535762
aysa-sy <aisa.soleimani83@gmail.com>
2023-06-14 18:47:38 +0330

    Add project files.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (Math.Sqrt(n) % 1 == 0)
+                Console.WriteLine("yes");
+            else
+                Console.WriteLine("n0");
+        }
+    }
+}
